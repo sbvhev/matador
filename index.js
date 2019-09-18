572c56f12d70486b4ca77feea4a4919f6aa8bd03
DevrajG <rohannepal200@gmail.com>
2019-09-18 03:08:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+import querystring from 'querystring';
+import _ from 'lodash';
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/api/unsubscribe', (req, res) => {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    data: querystring.stringify({
+      emails: req.body.email,
+      api_key: process.env.api_key,
+    }),
+    url: `https://a.klaviyo.com/api/v2/list/${process.env.list_id}/subscribe`,
+    timeout: 3000,
+  };
+  try {
+    axios(options);
+  } catch (err) {
+    console.log(err.response);
+  }
+});
+
+app.get('/api/check', async (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+    data: {},
+    url: `https://a.klaviyo.com/api/v2/group/${process.env.list_id}/members/all?api_key=${process.env.api_key}`,
+  };
+  try {
+    const { data: { records } = {} } = await axios(options);
+    const isChecked = !!_.find(records, { email: req.query.email });
+    res.json({ check: isChecked });
+  } catch (err) {
+    res.status(406).end();
+  }
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
